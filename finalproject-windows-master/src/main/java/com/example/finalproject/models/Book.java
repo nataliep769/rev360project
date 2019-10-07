8d37bae6f317ab0960d890dd47b21c1581287f65
Natalie Piontek <nataliep769@gmail.com>
2019-10-07 21:11:30 +0200

    Natalie is trying to make her application work again by adding imports.

---
+++ finalproject-windows-master/src/main/java/com/example/finalproject/models/Book.java
@@ -0,0 +1,32 @@
+package com.example.finalproject.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book {
+
+    public String name;
+
+    public String author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
