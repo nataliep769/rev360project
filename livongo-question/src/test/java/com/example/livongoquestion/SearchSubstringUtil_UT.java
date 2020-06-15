29ea58c985a0d4f4760b37fae56b7bc92db86379
Natalie Piontek <nataliep769@gmail.com>
2020-06-14 21:35:10 -0500

    Replaced existing tests with parameterized test

--- livongo-question/src/test/java/com/example/livongoquestion/SearchSubstringUtil_UT.java
+++ livongo-question/src/test/java/com/example/livongoquestion/SearchSubstringUtil_UT.java
@@ -1,8 +1,15 @@
 package com.example.livongoquestion;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import java.util.ArrayList;
 
 public class SearchSubstringUtil_UT {
 
@@ -13,17 +20,23 @@ public class SearchSubstringUtil_UT {
         assertThat(result, containsInAnyOrder("a", "ab", "abc", "b", "bc", "c"));
     }
 
-    @Test
-    public void findLongestRepeatedSubStringNoOverlap_valid() {
-        String result = SearchSubstringUtil.findLongestRepeatedSubStringNoOverlap("aabaabaaba");
-
-        assertThat(result, is(equalTo("aaba")));
+    @ParameterizedTest
+    @MethodSource("findLongestRepeatedSubstringArgs")
+    public void findLongestRepeatedSubStringNoOverlap(
+            String fullString,
+            String expectedSubstring
+    ) {
+        assertThat(
+                SearchSubstringUtil.findLongestRepeatedSubStringNoOverlap(fullString),
+                is(equalTo(expectedSubstring))
+        );
     }
 
-    @Test
-    public void findLongestRepeatedSubStringNoOverlapa_valid() {
-        String result = SearchSubstringUtil.findLongestRepeatedSubStringNoOverlap("nataliehinatalie");
-
-        assertThat(result, is(equalTo("natalie")));
+    private static Stream<Arguments> findLongestRepeatedSubstringArgs() {
+        return Stream.of(
+                Arguments.of("aabaabaaba", "aaba"),
+                Arguments.of("nataliehinatalie", "natalie"),
+                Arguments.of("banana", "na")
+        );
     }
 }
