ea26491ebae766ed65cff7930b5e9742f95e67b1
Natalie Piontek <nataliep769@gmail.com>
2020-07-12 19:41:14 -0500

    Formatting

--- livongo-question/src/test/java/com/example/codingprep/ListNodeUtil_UT.java
+++ livongo-question/src/test/java/com/example/codingprep/ListNodeUtil_UT.java
@@ -6,7 +6,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-
 public class ListNodeUtil_UT {
 
     @Test
